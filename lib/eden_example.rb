f4b52a5e1e126158c7168d9d2ab0426a71cf4991
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-26 10:48:39 +0100

    updated

---
+++ lib/eden_example.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "eden_example/version"
+
+module EdenExample
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/eden_example/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EdenExample
+  VERSION = "0.1.0"
+end
